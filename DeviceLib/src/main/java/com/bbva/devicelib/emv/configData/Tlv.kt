4d52cc759335890d7046e5209e2e584b9382e611
miguelangel.rodriguez.arevalo.contractor <miguelangel.rodriguez.arevalo.contractor@bbva.com>
2024-02-14 17:15:08 -0600

    se agregan las clases tlv y tlvList

--- DeviceLib/src/main/java/com/bbva/devicelib/emv/configData/Tlv.kt
+++ DeviceLib/src/main/java/com/bbva/devicelib/emv/configData/Tlv.kt
@@ -1,8 +1,142 @@
 package com.bbva.devicelib.emv.configData
 
+import com.bbva.utilitieslib.extensions.toHexaByte
 import com.bbva.utilitieslib.interfaces.IEmpty
 
-data class Tlv(var tag: String = "", var value: String = "") : IEmpty{
+
+const val FIRST_MASK = "81"
+const val SECOND_MASK = "82"
+const val CHARS_SIZE = 2
+
+const val FIRST_MAX_LENGTH = 127
+const val SECOND_MAX_LENGTH = 255
+const val MAX_LENGTH_SIZE = 6
+
+private val TAG = Constant.DEV_PREFIX + Tlv::class.java.simpleName
+private const val DEFAULT_PAD_CHAR = '0'
+
+
+data class Tlv(var tag: String = "", var value: String = ""): IEmpty {
+    constructor(tagData: TagData, char: Char = DEFAULT_PAD_CHAR): this(tagData.code, fillDefaultData(tagData, char))
+    constructor(tag: String, value: ByteArray): this(tag, convert(value))
+    constructor(tagData: TagData, value: ByteArray): this(tagData.code, value)
+    constructor(tagData: TagData, value: String): this(tagData.code, convert(tagData, value))
+    constructor(tagData: TagData, value: Int): this(tagData.code, convertToNumeric(tagData, value))
+
+    fun setValue(value: ByteArray) {
+        this.value = value.toHexaString()
+    }
 
     override fun isEmpty() = value.isEmpty()
+
+    fun pack() = StringBuilder()
+        .append(tag)
+        .append(calculateLength(value.length / 2))
+        .append(value).toString()
+
+    companion object {
+
+        private fun fillDefaultData(tagData: TagData, char: Char) =
+            String().padStart(tagData.maxLength * 2, char)
+
+        fun parser(tlv: String) = parser(tlv, 0).first
+
+        fun parser(tlv: String, startIndex: Int): Pair<Tlv, Int> {
+
+            var index = startIndex
+
+            val (tagAux, tagSize) = unpackTag(tlv, index)
+            index += tagSize
+
+            val (valueAux, valueSize) = unpackValue(tlv, index)
+            index += valueSize
+
+            return Pair<Tlv, Int>(Tlv(tagAux, valueAux), index - startIndex)
+        }
+
+
+        fun convertToNumeric(tagData: TagData, value: Int): String {
+            return if (tagData.format == EFormat.NUMERIC)
+                convert(tagData, value.toString())
+            else
+                throw NumberFormatException("$TAG -> Tag format wrong [$tagData.format]")
+        }
+
+        fun convert(tagData: TagData, value: String): String {
+            return when (tagData.format) {
+                EFormat.NUMERIC -> value.padStart(tagData.maxLength * 2, DEFAULT_PAD_CHAR)
+                else            -> value
+            }
+        }
+
+        fun convert(value: ByteArray) = value.toHexaString()
+
+        private fun calculateLength(valueLength: Int): String {
+            var fillLengthSize = 2
+            val lengthBuilder = StringBuilder(MAX_LENGTH_SIZE)
+
+            when {
+                valueLength > SECOND_MAX_LENGTH -> {
+                    lengthBuilder.append(SECOND_MASK)
+                    fillLengthSize = 4
+                }
+
+                valueLength > FIRST_MAX_LENGTH  -> {
+                    lengthBuilder.append(FIRST_MASK)
+
+                }
+            }
+
+            lengthBuilder.append(String.format("%0${fillLengthSize}x", valueLength))
+            return lengthBuilder.toString()
+        }
+
+        private fun unpackLength(data: String, startIndex: Int): Pair<Int, Int> {
+            var index: Int = startIndex
+            var length = data.substring(index, index + CHARS_SIZE)
+            index += CHARS_SIZE
+
+            if (length == FIRST_MASK) {
+                length = data.substring(index, index + CHARS_SIZE)
+                index += CHARS_SIZE
+            }
+            else if (length == SECOND_MASK) {
+                length = data.substring(index, index + CHARS_SIZE * 2)
+                index += CHARS_SIZE * 2
+            }
+
+            return Pair(length.toInt(16) * CHARS_SIZE, index - startIndex)
+        }
+
+
+        private fun unpackTag(data: String, startIndex: Int): Pair<String, Int> {
+            var index: Int = startIndex
+            val readMaxTags = 3
+            val tagSequence = java.lang.StringBuilder(4)
+
+            var tag = data.substring(index, index + CHARS_SIZE)
+            index += CHARS_SIZE
+            tagSequence.append(tag)
+
+            if ((tag.toHexaByte().toInt() and 0x1F) == 0x1F) {
+                for (point in 0 until readMaxTags) {
+                    val tagContinue = data.substring(index, index + CHARS_SIZE)
+                    index += CHARS_SIZE
+                    if ((tagContinue.toHexaByte().toInt() and 0x80) != 0x80) {
+                        tagSequence.append(tagContinue)
+                        break
+                    }
+                    tagSequence.append(tagContinue)
+                }
+            }
+            return Pair(tagSequence.toString(), index - startIndex)
+        }
+
+        private fun unpackValue(data: String, startIndex: Int): Pair<String, Int> {
+            val (length, size) = unpackLength(data, startIndex)
+            val index = startIndex + size
+            return Pair(data.substring(index, index + length), length + size)
+        }
+    }
 }
+
--- DeviceLib/src/main/java/com/bbva/devicelib/emv/configData/TlvList.kt
+++ DeviceLib/src/main/java/com/bbva/devicelib/emv/configData/TlvList.kt
@@ -1,4 +1,44 @@
 package com.bbva.devicelib.emv.configData
 
+import com.bbva.utilitieslib.extensions.toHexaString
+
+
 class TlvList(value: List<Tlv> = listOf()) {
+
+    private val list: MutableList<Tlv> = value.toMutableList()
+
+    fun add(value: Tlv) = list.add(value)
+    fun remove(value: Tlv) = list.remove(value)
+
+    fun find(tagData: TagData) = list.find { it.tag == tagData.code }
+    fun forEach(action: (Tlv) -> Unit) = list.forEach(action)
+
+    fun map(transform: (Tlv) -> String) = list.map(transform)
+
+    fun get(index: Int) = list[index]
+    fun set(index: Int, value: Tlv) = list.set(index, value)
+
+    operator fun plus(other: TlvList): TlvList = TlvList(list + other.list)
+
+    fun pack(): String {
+        val builder = StringBuilder()
+
+        list.forEach { tlv -> builder.append(tlv.pack()) }
+        return builder.toString()
+    }
+
+    companion object {
+
+        fun parser(tlvList: ByteArray) = parser( tlvList.toHexaString() )
+
+        fun parser(tlvList: String) = TlvList().apply {
+            var index = 0
+
+            while (index < tlvList.length) {
+                val pair = Tlv.parser(tlvList, index)
+                index += pair.second
+                list.add(pair.first)
+            }
+        }
+    }
 }
