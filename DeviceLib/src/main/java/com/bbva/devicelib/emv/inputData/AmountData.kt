af12beb583dcddd02b27f6ad99f7a342a4ae604d
miguelangel.rodriguez.arevalo.contractor <miguelangel.rodriguez.arevalo.contractor@bbva.com>
2024-04-08 13:53:29 -0600

    Se agregan nuevas funcionalidades, en el tlv para el pack tomar la longuitid en binario o Ascci. clase power se agrega el force reset y en info obtener el nivel de batería

--- DeviceLib/src/main/java/com/bbva/devicelib/emv/inputData/AmountData.kt
+++ DeviceLib/src/main/java/com/bbva/devicelib/emv/inputData/AmountData.kt
@@ -6,10 +6,10 @@ import com.bbva.utilitieslib.extensions.formatAmount
 data class AmountData(
     var amount: String = "0",
     var otherAmount: String = "0",
-    var currencyCode: String = Constant.MX_CURRENCY_CODE,
+    var currencyCode: String = "",
     var currencyExponent: Int = 2,
-    var symbol: String = Constant.MX_CURRENCY_SYMBOL,
-    var nDecimal: Int = Constant.NUMBER_OF_DECIMALS
+    var symbol: String = "",
+    var nDecimal: Int = 2
 ) {
 
     fun toFormatString(): String = amount.formatAmount(symbol, nDecimal)
