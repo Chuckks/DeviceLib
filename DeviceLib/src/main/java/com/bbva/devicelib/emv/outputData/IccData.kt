b18760261048bfc6091a883e4a57bffbb41fbc37
miguelangel.rodriguez.arevalo.contractor <miguelangel.rodriguez.arevalo.contractor@bbva.com>
2024-04-08 20:40:29 -0600

    Se agregan en la clase IccData nuevos valores como ATC y Readercapabilities

--- DeviceLib/src/main/java/com/bbva/devicelib/emv/outputData/IccData.kt
+++ DeviceLib/src/main/java/com/bbva/devicelib/emv/outputData/IccData.kt
@@ -5,7 +5,7 @@ import com.bbva.utilitieslib.interfaces.IEmpty
 private const val DEFAULT_TLV_RESULT = ""
 private const val DEFAULT_CRYPTOGRAM = ""
 private const val DEFAULT_APP_TX_COUNTER = ""
-private const val DEFAULT_CTLS_READER_CAP = ""
+private const val DEFAULT_FACTOR_INDICATOR = ""
 
 data class IccData(
 
@@ -13,7 +13,7 @@ data class IccData(
     var tlvResult: String = DEFAULT_TLV_RESULT,
     var cryptogram: String = DEFAULT_CRYPTOGRAM,
     var appTxCounter: String = DEFAULT_APP_TX_COUNTER,
-    var readerCapabilities: String = DEFAULT_CTLS_READER_CAP,
+    var factorIndicator: String = DEFAULT_FACTOR_INDICATOR,
     var tvr: Tvr = Tvr(),
     var tsi: Tsi = Tsi(),
     var cvmResult: CvmResult = CvmResult()
@@ -22,5 +22,5 @@ data class IccData(
 
     override fun isEmpty() = app.isEmpty() && cvmResult.isEmpty() && cryptogram == DEFAULT_CRYPTOGRAM
             && tlvResult == DEFAULT_TLV_RESULT && appTxCounter == DEFAULT_APP_TX_COUNTER
-            && readerCapabilities == DEFAULT_CTLS_READER_CAP
+            && factorIndicator == DEFAULT_FACTOR_INDICATOR
 }
